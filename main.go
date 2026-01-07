7b5c4ddf77491fae87fab535d0b722e851508bcb
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2026-01-07 02:14:22 -0300

    refactor main function to improve error handling and output formatting for file changes

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	changes, err := checkChangedFiles()
+	output, err := checkChangedFiles()
 	if err != nil {
-		fmt.Println("Error checking changed files")
+		fmt.Println("error checking changed files:", err)
 		return
 	}
-	if changes == "" {
-		fmt.Println("not changes detected")
+
+	changes := parseChangedFiles(output)
+
+	if len(changes) == 0 {
+		fmt.Println("no changes detected")
 		return
 	}
-	fmt.Println(changes)
+
+	for _, c := range changes {
+		fmt.Printf("[%c%c] %s\n", c.Index, c.Working, c.Path)
+	}
 }
